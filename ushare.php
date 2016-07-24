d64f277ca35605bfdf8b15cf611302ba20ef8f1f
Uğur KILCI <ugurbocugu8@gmail.com>
2016-07-24 18:25:35 +0300

    Update ushare.php

--- ushare.php
+++ ushare.php
@@ -18,9 +18,9 @@
 	
 	function ushare($usharea) {
 		$usharea = trim($usharea);
-		$ushb = array('fb','twt','gpls','pin','lin','vk','okru','su','rddt','tmblr','blggr','info');
+		$ushb = array('fb','twt','gpls','pin','lin','vk','okru','su','rddt','tmblr','blggr','i.n.f.o','uhstlnk');
 		$ushc = array("http://www.facebook.com/share.php?u=http://".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&ref=uShare","https://twitter.com/home?status=http://".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&ref=uShare","https://plus.google.com/share?url=http://".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&ref=uShare","https://pinterest.com/pin/create/button/?url=http://".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&?ref=uShare","https://www.linkedin.com/shareArticle?mini=true&url=http://".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&source=http://".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&ref=uShare","http://vk.com/share.php?url=".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&ref=uShare","http://www.odnoklassniki.ru/dk?st.cmd=addShare&st._surl=".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&ref=uShare","http://www.stumbleupon.com/submit?url=".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&ref=uShare","http://www.reddit.com/submit?url=http://".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&ref=uShare","http://www.tumblr.com/share/link?url=http://".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&ref=uShare","https://www.blogger.com/blog-this.g?u=http://".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&t=http://".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']."&ref=uShare",
-		"https://github.com/ugurkilci/uShare&ref=uShare");
+		"https://github.com/ugurkilci/uShare&ref=uShare","http://".$_SERVER['HTTP_HOST']."".$_SERVER['REQUEST_URI']");
 		$ushislem= str_replace($ushb,$ushc,$usharea);
 		return $ushislem;
 	}
